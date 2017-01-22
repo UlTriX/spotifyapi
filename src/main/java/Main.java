08c3dd54ee321a09a3aea1de2ad08f6de38d8806
Viur Laptop <Viur Laptop>
2017-01-22 17:20:05 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Asus Laptop on 22-Jan-17.
+ */
+public class Main {
+
+    public List<String> artists = Arrays.asList("Led Zeppelin","Pink Floyd");
+
+    public static void main(String args[]) {
+
+
+
+    }
+}
